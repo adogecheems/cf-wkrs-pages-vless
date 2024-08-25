ab101045c47258f563249a06e3fca64afc3a6792
Lingi Sheyorz <77494973+adogecheems@users.noreply.github.com>
2024-08-25 16:02:15 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'dad64cd4-ac37-43f8-ba5f-677aadb5e7b6';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
